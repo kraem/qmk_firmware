e13bc1c6f1f5f172587fb119ec6526902d2024a3
kraem <ebrin.ronnie@protonmail.com>
2021-04-12 21:24:26 +0200

    kraem/lily58: home row mods

--- keyboards/lily58/keymaps/kraem/config.h
+++ keyboards/lily58/keymaps/kraem/config.h
@@ -32,8 +32,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
+//#define TAPPING_FORCE_HOLD
+//#define TAPPING_TERM 100
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
+
+// Left-hand home row mods
+#define HOME_A LALT_T(KC_A)
+#define HOME_S LGUI_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L RGUI_T(KC_L)
+#define HOME_SCLN RALT_T(KC_SCLN)
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
